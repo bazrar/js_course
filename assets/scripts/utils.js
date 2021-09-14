2bd7beba258e10567de3615dda084472cbd98c78
rejan bajracharya <rejanbajracharya@bajratechnologies.com>
2021-09-14 09:55:39 +0545

    render current operation

--- assets/scripts/utils.js
+++ assets/scripts/utils.js
@@ -1,29 +1,36 @@
-function outputResult(value) {
-    currentResult.textContent = value; 
+function outputResult(currentValue, currentDescription) {
+    currentCalcuation.textContent = currentDescription; 
+    currentResult.textContent = currentValue;
 }
+
 function calculation(operator) {
     const input = parseInt(userInput.value); 
     const currentValue = parseInt(currentResult.textContent);
+    let currentDescription;
     let res = 0; 
     switch(operator) { 
         case '+': 
+        currentDescription = `${currentValue} + ${input}`;
         res = currentValue + input; 
-        outputResult(res)
+        outputResult(res, currentDescription);
         break; 
 
         case '-': 
+        currentDescription = `${currentValue} - ${input}`;
         res = currentValue - input; 
-        outputResult(res)
+        outputResult(res, currentDescription);
         break;
 
         case '*': 
+        currentDescription = `${currentValue} * ${input}`;
         res = currentValue * input; 
-        outputResult(res)
+        outputResult(res, currentDescription);   
         break;
 
         case '/': 
+        currentDescription = `${currentValue} / ${input}`;
         res = currentValue / input; 
-        outputResult(res)
+        outputResult(res, currentDescription);
         break;
     }
 }
